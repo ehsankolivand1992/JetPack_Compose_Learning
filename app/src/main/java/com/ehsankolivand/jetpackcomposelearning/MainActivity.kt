5a357df4eec6ecdeca7201f75d44928f4576917a
ehsankolivand <ehsankolivandeh@gmail.com>
2021-08-27 13:50:20 +0430

    make login screen

--- app/src/main/java/com/ehsankolivand/jetpackcomposelearning/MainActivity.kt
+++ app/src/main/java/com/ehsankolivand/jetpackcomposelearning/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ehsankolivand.jetpackcomposelearning
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -8,8 +10,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -70,15 +71,60 @@ class MainActivity : ComponentActivity() {
                 Card(
                     shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp),
                     backgroundColor = MaterialTheme.colors.background,
-                    modifier = Modifier.fillMaxSize().padding(top = 50.dp)
-                        .constrainAs(loginForm){
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 50.dp)
+                        .constrainAs(loginForm) {
                             bottom.linkTo(loginForm.bottom)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
                         },
                     elevation = 10.dp
                 ) {
+                    ConstraintLayout(modifier = Modifier.background(MaterialTheme.colors.background)) {
+                        val (refuser,refpass,refbutton,img) = createRefs()
 
+                       Image(painter = painterResource(id = R.drawable.img), contentDescription ="logo",Modifier.constrainAs(img){
+                           top.linkTo(parent.top)
+                           bottom.linkTo(refuser.top)
+                           start.linkTo(parent.start)
+                           end.linkTo(parent.end)
+                       } )
+                        TextField(value = "", onValueChange = {},
+                        modifier = Modifier
+                            .constrainAs(refuser)
+                            {
+                                top.linkTo(img.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }
+                            .fillMaxWidth()
+                            .padding(9.dp, 20.dp, 9.dp, 10.dp)
+                        )
+                        TextField(value = "", onValueChange = {},
+                            modifier = Modifier
+                                .constrainAs(refpass)
+                                {
+                                    top.linkTo(refuser.bottom)
+                                    start.linkTo(parent.start)
+                                    end.linkTo(parent.end)
+                                }
+                                .fillMaxWidth()
+                                .padding(9.dp, 0.dp, 9.dp, 10.dp),
+
+                        )
+                        Button(
+                            onClick = { Toast.makeText(this@MainActivity,"salam",Toast.LENGTH_LONG).show() },
+                            modifier = Modifier.constrainAs(refbutton) {
+                                top.linkTo(refpass.bottom)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }) {
+                            Text(text = "Login")
+                        }
+
+
+                    }
                 }
             }
 
@@ -94,6 +140,9 @@ class MainActivity : ComponentActivity() {
             LoginBody()
         }
     }
+
+
 }
+
 
 
